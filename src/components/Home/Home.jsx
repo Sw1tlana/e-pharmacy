401f8773c571ae4c35c61eee834aeec51c70e0bf
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-11-17 23:43:47 +0200

    added Home image, title

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,16 @@
 import style from './Home.module.css';
 import Container from '../../shared/components/Container/Container';
+import exampleImage from '../../shared/images/homePage/capsule@2х.png';
 
 function Home() {
   return (
     <section className={style.homeSection}>
- <p>Hello</p>
+      <Container>
+        <div className={style.customer}>
+          <h1 className={style.titleHome}>Your medication delivered</h1>
+          <img src={exampleImage} alt="example" />
+        </div>
+      </Container>
     </section>
   )
 }

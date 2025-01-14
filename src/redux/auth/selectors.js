48057e348f2d231a5063bdd8d6f1ae0004434367
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2025-01-14 13:08:29 +0200

    changes selectors

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,14 +1,14 @@
-export const selectUser = ((state) => state.auth.user);
+export const selectUser = (state) => state.auth.user;
 console.log('Current User:', selectUser);
 
-export const selectEmail = ((state) => state.auth.email);
+export const selectEmail = (state) => state.auth.user?.email;
 
-export const selectToken = ((state) => state.auth.token);
+export const selectToken = (state) => state.auth.token;
 
-export const selectIsLoggedIn = ((state) => state.auth.isLoggedIn);
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 console.log('Is logged in:', selectIsLoggedIn);
 
-export const selectIsRefreshing = ((state) => state.auth.isRefreshing);
+export const selectIsRefreshing = (state) => state.auth.isRefreshing;
 
-export const selectAutchError = ((state) => state.auth.error);
+export const selectAutchError = (state) => state.auth.error;
 
